f4788d2e274a135cee7b6b37d25b0d95b0f6542d
trentonamasa <36996745+trentonamasa@users.noreply.github.com>
2018-10-23 21:07:53 -0600

    Update the files and finish the RL Program

--- CartAndPole.py
+++ CartAndPole.py
@@ -5,6 +5,10 @@
 import tensorflow as tf
 import numpy as np
 import math
+import random
+import gym
+import matplotlib.pyplot as plt
+
 
 #GLOBAL CONSTANTS
 FORCE_MAG = 10
@@ -15,34 +19,110 @@ TAU = 0.02
 GRAVITY = 9.8
 LENGTH = 0.326
 FOURTHIRDS = 4/3
+GAMMA = 0.5
+ALPHA = 0.1
+
+lookUpTable = []
+for action in range(0,2):
+    lookUpTable.append([])
+    for theta in range(0,6):
+        lookUpTable[action].append([])
+        for thetaDot in range(0,3):
+            lookUpTable[action][theta].append([])
+            for x in range(0,3):
+                lookUpTable[action][theta][thetaDot].append([])
+                for xDot in range(0,3):
+                    lookUpTable[action][theta][thetaDot][x].append(random.randint(0,51))
+                
 
 class state:
     def __init__(self, x, xDot, theta, thetaDot):
-        self.x = x
-        self.xDot = xDot
         self.theta = theta
         self.thetaDot = thetaDot
+        self.x = x
+        self.xDot = xDot
 
 
-def main():
-    currState = state(0, 1, 0, 1)
-    action = True # True = right, False = left
-    iterator = 0
 
-    while ((currState.theta > -90 and currState.theta < 90) and (currState.x > -10 and currState.x < 10)):
-        cartPole(action, currState)
+def main():    
+    attempt = 0
+    listofAttempts = []
+    while attempt < 100:
+        iterator = 0
+        currState = state(0, 0, 0, 0)
+        action = True # True = right, False = left
+        inBounds = True
+        while (inBounds):
+            action = argMaxQ(currState)
+            updateQTable(action, currState)
+            cartPoleReaction(action, currState)
+            inBounds = checkInBounds(currState)
+            iterator += 1
+            # print("Action: ", action, "     x: ", round(currState.x, 1), "        xDot: ", round(currState.xDot, 1), "      theta: ", round(currState.theta, 1), "    thetaDot: ", round(currState.thetaDot, 1), "      Iteration: ", iterator)
+        attempt += 1
+        print("Attempt: ", attempt, "   Iterations: ", iterator)
+        listofAttempts.append(iterator)
 
-        if (currState.thetaDot < -1):
-            action = False
-        elif(currState.thetaDot > 1):
-            action = True
+    plt.plot(listofAttempts)
+    plt.title('Attempts vs. Iterations')
+    plt.xlabel('Attempts')
+    plt.ylabel('number of iterations')
+    plt.show()
 
-        iterator += 1
-        print("Action: ", action, "\nx: ", currState.x, "\nxDot: ", currState.xDot, "\ntheta: ", currState.theta,
-             "\nthetaDot: ", currState.thetaDot, "\nIteration: ", iterator, "\n")
+    
 
 
-def cartPole(action, currState):
+
+def checkInBounds(currState):
+    if thetaToCatagory(currState) == 6 or thetaDotToCatagory(currState) == 3 or xToCatagory(currState) == 3 or xDotToCatagory(currState) == 3:
+        return False
+    else:
+        return True
+
+
+def updateQTable(action, currState):
+    lookUpTable[int(action)][thetaToCatagory(currState)][thetaDotToCatagory(currState)][xToCatagory(currState)][xDotToCatagory(currState)] += ALPHA*(calcReward(currState) + GAMMA*(maxQ(action, currState)) - lookUpTable[int(action)][thetaToCatagory(currState)][thetaDotToCatagory(currState)][xToCatagory(currState)][xDotToCatagory(currState)])
+
+
+def calcReward(currState):
+    reward = 0
+
+    if currState.theta >= -12 and currState.theta < -6:
+        reward += 1
+    elif currState.theta >= -6 and currState.theta < -1:
+        reward += 2
+    elif currState.theta >= -1 and currState.theta < 0:
+        reward += 4
+    elif currState.theta >= 0 and currState.theta < 1:
+        reward += 4
+    elif currState.theta >= 1 and currState.theta < 6:
+        reward += 2
+    elif currState.theta >= 6 and currState.theta <= 12:
+        reward += 1
+
+    if currState.x >= -2.4 and currState.x < -0.8:
+        reward += 1
+    elif currState.x >= -0.8 and currState.x < 0.8:
+        reward += 3
+    elif currState.x >= 0.8 and currState.x <= 2.4:
+        reward += 1
+    
+    return reward
+
+
+def maxQ(action, currState):
+    ifLeft = qTable(False, currState)
+    ifRight = qTable(True, currState)
+    return max(ifLeft,ifRight)
+
+
+def argMaxQ(currState):    
+    ifLeft = qTable(False, currState)
+    ifRight = qTable(True, currState)
+    return False if ifLeft > ifRight else True
+
+
+def cartPoleReaction(action, currState):
     force = FORCE_MAG if action > 0 else - FORCE_MAG
     temp = (force+POLEMASS_LENGTH*currState.thetaDot**2*math.sin(currState.theta))/TOTALMASS
     thetaAcc = (GRAVITY*math.sin(currState.theta) - math.cos(currState.theta)*temp)/(LENGTH*(FOURTHIRDS-MASSPOLE*(math.cos(currState.theta))**2*TOTALMASS))
@@ -56,5 +136,61 @@ def cartPole(action, currState):
     return currState
 
 
-if __name__=="__main__":
-    main()
+def qTable(action, currState):
+    value = lookUpTable[int(action)][thetaToCatagory(currState)][thetaDotToCatagory(currState)][xToCatagory(currState)][xDotToCatagory(currState)]
+    return value
+
+
+def thetaToCatagory(currState):
+    if currState.theta >= -12 and currState.theta < -6:
+        return 0
+    elif currState.theta >= -6 and currState.theta < -1:
+        return 1
+    elif currState.theta >= -1 and currState.theta < 0:
+        return 2
+    elif currState.theta >= 0 and currState.theta < 1:
+        return 3
+    elif currState.theta >= 1 and currState.theta < 6:
+        return 4
+    elif currState.theta >= 6 and currState.theta <= 12:
+        return 5
+    else:
+        return 6
+
+
+def thetaDotToCatagory(currState):
+    infinity = 1000000
+    if currState.thetaDot >= -infinity and currState.thetaDot < -50:
+        return 0
+    elif currState.thetaDot >= -50 and currState.thetaDot < 50:
+        return 1
+    elif currState.thetaDot >= 50 and currState.thetaDot <= infinity:
+        return 2
+    else:
+        return 3
+    
+
+def xToCatagory(currState):
+    if currState.x >= -2.4 and currState.x < -0.8:
+        return 0
+    elif currState.x >= -0.8 and currState.x < 0.8:
+        return 1
+    elif currState.x >= 0.8 and currState.x <= 2.4:
+        return 2
+    else:
+        return 3
+
+
+def xDotToCatagory(currState):
+    infinity = 1000000
+    if currState.x >= -infinity and currState.x < -0.5:
+        return 0
+    elif currState.x >= -0.5 and currState.x < 0.5:
+        return 1
+    elif currState.x >= 0.5 and currState.x <= infinity:
+        return 2
+    else:
+        return 3
+
+
+main()
